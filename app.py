26e72c4d817fdbed265443b91cf1767f46dceea8
eduardogabrielz <eduardogabrielz2002@gmail.com>
2024-03-26 22:06:52 -0300

    Adicionamento de tradução

--- app.py
+++ app.py
@@ -11,6 +11,36 @@ character = ['Goku', "vegeta", "Piccolo", "Bulma", "Freezer", "Zarbon", "Dodoria
              "Kaio-shin del Este", "Kaio-shin del Norte", "Kaio-shin del Sur", "Kaio-shin del Oeste", "Gran Kaio-shin",
              "Kibito"]
 
+translation_planets = {
+    "Tierra" : "Terra",
+    "Planeta de Bills " : "Planeta do Bills",
+    "Nucleo del Mundo" : "Núcleo do Mundo",
+    "Planeta del Gran Kaio" : "Planeta do Grande Senhor Kaio",
+    "Freezer No. 79" : "Planeta Freeza 79",
+    "Otro Mundo" : "Outro Mundo",
+    "Kaiō del Norte" : "Planeta do Senhor Kaioh",
+    "Planeta sagrad": "Planeta Sagrado de Kaiohshin",
+}
+
+translation_genders = {
+    "Male" : "Homen",
+    "Female" : "Mulher",  
+}
+
+translation_race = {
+    "God" : "Deus",
+    "Saiyan" : "Saiyajin",
+    "Angel" : "Anjo",
+    "Nucleico" : "Shin-jin",
+    "Evil" : "Demonio",
+    "Namekian" : "Namekuseijin",
+    "Human" : "Humano",
+    "Frieza Race" : "Raça do Freeza",
+    "Unknown" : "Desconhecido",
+    "Jiren Race" : "Humanoide",
+    "Nucleico benigno" : "Shin-jin"
+}
+
 
 app = Flask(__name__)
 
@@ -19,31 +49,31 @@ def jogar():
     
     base_url = "https://dragonball-api.com/api/characters/"   
     
-    ids_invalidos = [62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 41, 36]
+    invalid_ids = [62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 41, 36]
 
     while True:
-        numero_aleatorio = random.randint(1, 78)
+        random_number = random.randint(1, 78)
 
-        if numero_aleatorio not in ids_invalidos:
+        if random_number not in invalid_ids:
             break
 
-    url_personagem = base_url + str(numero_aleatorio)
+    url_character = base_url + str(random_number)
 
-    repost = requests.get(url_personagem)
-    personagem = repost.json()
+    repost = requests.get(url_character)
+    characters = repost.json()
     
-    resultado = {}
+    result_character = {}
     
-    resultado["afiliacao"] = personagem["affiliation"]
-    resultado["genero"] = personagem["gender"]
-    resultado["imagem"] = personagem["image"]
-    resultado["nome"] = personagem["name"]
-    resultado["raca"] = personagem["race"]
+    result_character["afiliação"] = characters["affiliation"]
+    result_character["genero"] = characters["gender"]
+    result_character["imagem"] = characters["image"]
+    result_character["nome"] = characters["name"]
+    result_character["raça"] = characters["race"]
 
-    planeta_origem = personagem["originPlanet"]
-    resultado["planeta"] = planeta_origem["name"] if planeta_origem else None
+    origin_planet = characters["originPlanet"]
+    result_character["planeta"] = origin_planet["name"] if origin_planet else None
 
-    return jsonify(resultado)
+    return jsonify(result_character)
 
 if __name__ == '__main__':
   app.run(port=5000, host='localhost', debug=True)
