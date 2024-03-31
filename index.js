e49b7a560b1571531bfcf941831f82414436bcba
eduardogabrielz <eduardogabrielz2002@gmail.com>
2024-03-31 14:15:35 -0300

    att javascript

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const characters = ['Goku', "Vegeta", "Piccolo", "Bulma", "Freeza", "Zarbon", "D
 const resultBox = document.querySelector(".result_box")
 const inputBox = document.getElementById("searchBox")
 const guessButton = document.querySelector(".guess_character")
+let character = "";
+let tip_character = "";
+let responseLineCount = 0;
 
 inputBox.onkeyup = function () {
   let result = []
@@ -39,6 +42,64 @@ function selectInput(list) {
   inputBox.value = list.innerHTML;
   resultBox.innerHTML = "";
 }
+
+function guess_character(character){
+  
+  responseLineCount++;
+  
+  const div_image = document.createElement('div');
+  div_image.classList.add('individual_response_line');
+
+  const div_name = document.createElement('div');
+  div_name.classList.add('individual_response_line');
+
+  const div_gender = document.createElement('div');
+  div_gender.classList.add('individual_response_line');
+
+  const div_race = document.createElement('div');
+  div_race.classList.add('individual_response_line');
+
+  const div_afiliation = document.createElement('div');
+  div_afiliation.classList.add('individual_response_line');
+  
+  const imageCharacter = document.createElement('img');
+  imageCharacter.classList.add('image_character');
+  imageCharacter.src = character.imagem;
+  imageCharacter.alt = "Personagem do palpite";
+
+  const nameCharacter = document.createElement('p');
+  nameCharacter.classList.add('tip_character');
+  nameCharacter.textContent = character.nome;
+
+  const genderCharacter = document.createElement('p');
+  genderCharacter.classList.add('tip_character');
+  genderCharacter.textContent = character.genero;
+
+  const raceCharacter = document.createElement('p');
+  raceCharacter.classList.add('tip_character');
+  raceCharacter.textContent = character.raça;
+
+  const afiliationCharacter = document.createElement('p');
+  afiliationCharacter.classList.add('tip_character');
+  afiliationCharacter.textContent = character.afiliação;
+
+  div_image.appendChild(imageCharacter);
+  div_name.appendChild(nameCharacter);
+  div_gender.appendChild(genderCharacter);
+  div_race.appendChild(raceCharacter);
+  div_afiliation.appendChild(afiliationCharacter);
+
+  const responseLineDiv = document.querySelector(`.response_line${responseLineCount}`);
+  
+  responseLineDiv.appendChild(div_image);
+  responseLineDiv.appendChild(div_name);
+  responseLineDiv.appendChild(div_gender);
+  responseLineDiv.appendChild(div_race);
+  responseLineDiv.appendChild(div_afiliation);
+
+  responseLineDiv.style.display = 'flex';
+}
+
 function iniciarGame() {
 
   const xhr = new XMLHttpRequest();
@@ -56,7 +117,7 @@ function iniciarGame() {
       document.querySelector(".search_box").style.display = "block";
       document.querySelector(".header").style.display = "flex";
       document.querySelector(".header_content_characteristics").style.display = "flex";
-      document.querySelector(".response_line").style.display = "flex";
+      character = resposta;
       console.log(resposta);
     } else {
       console.error('Erro na requisição:', xhr.status, xhr.statusText);
@@ -78,10 +139,13 @@ guessButton.addEventListener("click", function () {
   xhr.onload = function () {
     if (xhr.status === 200) {
       const resposta = JSON.parse(xhr.responseText);
-      console.log(resposta);
+      tip_character = resposta;
       searchBox.value = "";
+      console.log(tip_character)
+      guess_character(tip_character);
     } else {
       console.error('Erro na requisição:', xhr.status, xhr.statusText);
     }
   };
 });
+
