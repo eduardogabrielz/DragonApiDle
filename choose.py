b65dccb8d1494bb98ad1e40c6b0739029b29ae14
eduardogabrielz <eduardogabrielz2002@gmail.com>
2024-05-28 11:51:47 -0300

    att

---
+++ choose.py
@@ -0,0 +1,26 @@
+def choose_features(characters):
+    
+    result_character = {}
+  
+    result_character["nome"] = characters["name"]
+    result_character["genero"] = characters["gender"]
+    result_character["raça"] = characters["race"]
+    result_character["afiliação"] = characters["affiliation"]
+    result_character["imagem"] = characters["image"]
+    result_character["Ki"] = characters["ki"]
+
+    return result_character
+
+def choose_features_tips(characters):
+
+  character_data = characters[0]  
+
+  result_character = {}
+  result_character["nome"] = character_data["name"]
+  result_character["genero"] = character_data["gender"]
+  result_character["raça"] = character_data["race"]
+  result_character["afiliação"] = character_data["affiliation"]
+  result_character["imagem"] = character_data["image"]
+  result_character["Ki"] = character_data['ki']
+
+  return result_character
